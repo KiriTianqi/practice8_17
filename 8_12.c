434025e05ee61efd4245353a7279cad3302a4931
liutianqiJoey <88476491+liutianqiJoey@users.noreply.github.com>
2021-08-17 17:00:09 +0800

    Add files via upload

---
+++ 8_12.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	do
+	{
+		i++;
+		printf("%d ", i);
+		//i/*++;*/
+	} while (i < 10);
+		/*int i = 0;
+		int k = 0;
+		for (i = 0, k = 0; k = 0; i++, k++)
+			k++;*///recircle 0 time
+
+	
+	//int i = 1;
+	//while (i <=10)
+	//{
+	//	i++;
+	//	if (i == 5)
+	//		continue;
+	//	printf("%d ", i-1);
+	/*	i++;*/
+	//}
+	/*for (i = 1; i <= 10; i++)
+	{
+		if (i == 10)
+			continue;
+		printf("%d ", i);
+		}*/
+	return 0;
+ }
